5f7b895bdbf05ee9ed86f432109b108d14a54fa4
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 22:23:11 +0500

    added tuple.py

---
+++ tuple.py
@@ -0,0 +1,10 @@
+# a tuple is an immutable data type in python.
+a=(1,2,3,4,6,8,)
+print(type(a))
+
+a=()#is an empty tuple
+print(a)
+a=(1,)#single value tuple is always followed by a comma
+print(a)
+
+
