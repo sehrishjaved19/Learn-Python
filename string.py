929b8b13a788e4e797eca5e2511a69e1dc591911
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 19:22:38 +0500

    added string.py

---
+++ string.py
@@ -0,0 +1,11 @@
+#string is a scequence of characters enclosed in quotes
+
+#string is immutable!
+
+
+a='sehrish'  #is a quoted string
+b="hello world"   # is a double quoted string
+c='''one of the hardest thing to say in life is hello for
+ the first time and good bye for the last time !
+   another quote is
+     A thing of beauty is a joy forever'''   #this is a multi-line string
