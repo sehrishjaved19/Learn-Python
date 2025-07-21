ff80c851ad6614ae59e786f3583b495cea0c7371
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 19:53:09 +0500

    added slicing_string.py

---
+++ slicing_string.py
@@ -0,0 +1,22 @@
+name='Sehrish'
+nickname=name[0:5]
+print(nickname)#will print index 0 to 5 excluding index5 
+
+print(name[-4:-1])#this is negative slicing
+
+
+character1=name[3] 
+print(character1)#will print the character from the string at index3
+
+character=name[:5]
+print(character)#is same as [0:5]
+
+character=name[3:]
+print(character)#is same as [3:7]
+
+
+
+
+
+
+
