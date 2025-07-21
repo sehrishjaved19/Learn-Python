38cca2b1133eb6c0e60420ab9ba2de82bbd17f3c
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 20:40:38 +0500

    added ch3ps3.py
    chapter 3, practice set 3

---
+++ ch3ps3.py
@@ -0,0 +1,3 @@
+name='sehrish is a  learner!'
+print(name.find("  "))
+#if the double space will be present in the string the output will be the indexbalue of the double space othervise it will print -1.
