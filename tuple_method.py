948bccbb0a38a2d00415cb3153e7d174be7d3c7d
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 22:38:41 +0500

    added tuple_method.py

---
+++ tuple_method.py
@@ -0,0 +1,8 @@
+a=('hello',False,35, False,65.78,False,'newage')
+print(a)
+
+no=a.count(False)
+print(no)#use to display how many time a particukar value is present in the tuple.as False is 3 times present in the tuple, it will display 3 as output
+
+i=a.index(False)
+print(i)#it will returns the first index of a particular value
