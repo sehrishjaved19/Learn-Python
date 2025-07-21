5f286dae606d48f4ed8600ec66381f9d258cbfac
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 20:28:51 +0500

    added

--- ch3ps1.py
+++ ch3ps1.py
@@ -1,4 +1,10 @@
 name=input('Enter Your Name: ')
-print(f'Good Afternoon,{name}')
+print(f'Good Afternoon,{name}\n')
 
 #this is an f string which is used to embed expressiuons or variables directly into the strings,making string formatting cleaner and more readable. 
+
+name = "Sehrish"
+age = 19
+
+# Using an f-string
+print(f"My name is {name} and I am {age} years old.")
