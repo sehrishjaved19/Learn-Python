6bb9fd3464b04ee137b2a48971d8fa30e8914d5b
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-22 00:05:03 +0500

    added dict.py

---
+++ dict.py
@@ -0,0 +1,11 @@
+#a dict (short for dictionary) is a built-in data type that stores data as key–value pairs.
+ 
+#   dict is mutable
+#kets must be unique and imutable
+
+marks={'aiman':80,
+       'sehrish':90,
+       'zara':80}
+print(marks,type(marks))
+
+print(marks['zara'])#indexed by keys, not by index number
