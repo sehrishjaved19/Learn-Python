b92ccb300a84693838873e5318c2f8d87f048ac6
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 18:52:51 +0500

    Update typecast.py

--- typecast.py
+++ typecast.py
@@ -5,3 +5,15 @@ print(t)
 b=23.4
 u=type(b)
 print(u)
+
+learner = "SEHRISH JAVED"
+print(type(learner))
+
+z ="31.2"
+print(type(z)) #type will be string.
+
+n='45'
+q=float(n)
+f=type(q)
+print(f)#explicit type casting
+
