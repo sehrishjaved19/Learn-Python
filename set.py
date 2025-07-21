8f24af09251b0184220f95ad9a1c9782e79649a0
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-22 01:08:03 +0500

    added set.py

---
+++ set.py
@@ -0,0 +1,12 @@
+#set is a collection of non-repetitive elements
+s={1,2,3}
+print(s)
+s.add(456)
+print(s)
+e=set()#this is an empty set, dont use s={},because it create an empty dictionary
+print(type(e))
+
+#set are unordered
+#sets are unindexed(cannot access elements by index)
+#there is no way to change items in sets
+#sets cannot contain duplicate values
