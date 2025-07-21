3c4ba6ff639fba45b72b9c770b65e53fd7d18f95
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 20:02:49 +0500

    added fn_string.py

---
+++ fn_string.py
@@ -0,0 +1,13 @@
+name='sehrish'
+print(len(name))#use to get the lenght of the string
+
+print(name.endswith('rish'))#will show true
+
+print(name.startswith('seh'))#this will also print true
+
+print(name.capitalize())# THIS WILL CAPATALIZE THE FIRST LETTER OF THE STRING
+
+print(name.replace('sehrish','shero'))#thos will replace the value
+
+
+
