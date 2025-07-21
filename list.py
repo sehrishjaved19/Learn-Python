0591cae35289d6d17578ad6744c23aa3db75b2ef
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 20:56:59 +0500

    added list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+#A list is a built-in data structure in Python that is used to store multiple items in a single variable.
+friends=['apple','3.14','aiman',False,'new']
+print(friends[0])#this will print the value at index0
+
+friends[0]='grapes'#this will replace the value of index0.
+
+print(friends)#list is mutable
+
+print(friends[1:4])
+
