b117d65f8140b333071e3a774415d8046fbfc36e
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-22 01:56:52 +0500

    added ch5ps4.py

---
+++ ch5ps4.py
@@ -0,0 +1,17 @@
+d={}
+name=input('Enter friend1 name:')
+lang=input('Enter language name: ')
+d.update({name:lang})
+
+name=input('Enter friend2 name:')
+lang=input('Enter language name: ')
+d.update({name:lang})
+
+name=input('Enter friend3 name:')
+lang=input('Enter language name: ')
+d.update({name:lang})
+
+name=input('Enter friend4 name:')
+lang=input('Enter language name: ')
+d.update({name:lang})
+print(d)
