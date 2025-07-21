6e4366153896210a28a9a3d78d6cb56908444fcf
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 20:25:42 +0500

    added ch3ps1.py

---
+++ ch3ps1.py
@@ -0,0 +1,4 @@
+name=input('Enter Your Name: ')
+print(f'Good Afternoon,{name}')
+
+#this is an f string which is used to embed expressiuons or variables directly into the strings,making string formatting cleaner and more readable. 
