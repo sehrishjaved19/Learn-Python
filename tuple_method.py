bd78f8ce74a60a4b964d1d0435a1a2f2577e3ba2
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-21 22:55:45 +0500

    modidied tuple method
    added more methods

--- tuple_method.py
+++ tuple_method.py
@@ -10,3 +10,15 @@ print(i)#it will returns the first index of a particular value
 repeat=(1,2,3)
 r=repeat*5#this will print the value of repeat 5 time
 print(r)
+
+tuple1=(1,2,3)
+tuple2=(4,5,6)
+s=tuple1+tuple2
+print(s)#this will join these two tuples into one tuple
+
+q=(35,50,60,90,'hello')
+print(60 in q)#this will give boolean falue either 60 is present in q or not
+
+q=(35,50,60,90,'hello')
+print(len(q))#give length of tuple
+
