0d3e2a19126abdac63672ca22d87267be90cea1d
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-22 14:29:40 +0500

    added ch6ps5.py
    chapter6 practice set5

---
+++ ch6ps5.py
@@ -0,0 +1,6 @@
+lst=['aiman','fatimah','sehrish','farah']
+name=input('Enter your name: ')
+if(name in lst):
+    print('Your name is in the list!')
+else:
+    print('Your name is not in the list!')
