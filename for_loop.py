334fa0c4f044d9cff1cef5df1ad2e11ad9657c22
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-23 00:04:34 +0500

    added for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,40 @@
+# A FOR LOOP is used to iterate through a sequence like list, tuple, or string[iterables]
+
+# The range() function in pyhton is used to generatw a sequence of number
+#range(start, stop, step_size)
+
+for i in range(0, 4):
+    print(i)
+#Another way
+for i in range(4):
+    print(i)
+
+
+
+#for loop for list
+lst=[1,4,6,234,6,764]
+for i in lst:
+    print(i)
+#i is a loop variable that refers to each item in the list, one by one.
+
+
+
+
+#for loop for tuple
+tup=(1,4,5,876,45)
+for items in tup:
+    print(items)
+
+
+
+
+
+#for loop for string with else
+s='sehrish'
+for i in s:
+    print(i)
+else:print('done!')#this will be printed when loop is exhausts or come to an end!
+
+
+
+
