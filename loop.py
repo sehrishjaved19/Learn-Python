4a52e112352aadb35e9200a8f077e81709dd4a5f
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-22 20:24:34 +0500

    added loop.py
    Introduction to loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+#loops are used to repeat the instrustions.
+
+#there are two tyoes of loop in python
+
+#    1-->    while loop
+#    2-->    for loop
+
+#e.g:
+
+for i in range(1, 6):
+    print(i)
+#this will print the counting from 1 to 5
+
