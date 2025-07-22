ea1a82b4905fceaea31ee2919cf06a2732cbe401
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-22 14:24:23 +0500

    added ch6ps4.py
    chapter6 practice set4

---
+++ ch6ps4.py
@@ -0,0 +1,6 @@
+#write a program to check whether a given username contains less than 10 characters or not?
+user_name = input('Enter your user name: ')
+if(len(user_name) <= 10):
+    print('Your username contaions less than or equal to 10 characters!')
+else:
+    print('Your user name contains characters more than 10!')
