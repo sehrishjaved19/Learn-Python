ac290a66db84e06f9c74c096703174fbd401636b
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-23 00:37:05 +0500

    added pass_statement.py

---
+++ pass_statement.py
@@ -0,0 +1,13 @@
+# pass is a null statement in python
+#it instruts to do nothing
+
+for i in range(645):
+    pass#without pass the program throw an error
+
+
+
+
+
+while(i<45):
+    print(i)
+    i += 1
