834b586e5fb73feae82a99730b24ecb5ca3f94fa
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-24 21:43:02 +0500

    added Recursion.py

---
+++ Recursion.py
@@ -0,0 +1,10 @@
+#Recursion is a programming technique where a function calls itself to solve a problem.
+#Instead of using loops, recursion breaks a problem into smaller sub-problems of the same type and solves them using repeated function calls.
+
+def factorial(n):
+    if n == 0 or n == 1:  # Base case
+        return 1
+    else:
+        return n * factorial(n - 1)  # Recursive call
+
+print(factorial(5))  # Output: 120
