f6a2aee3854846a2a2967cb83ce889520b3fda10
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-24 22:11:37 +0500

    added ch8ps2.py

---
+++ ch8ps2.py
@@ -0,0 +1,9 @@
+#write a python program using function to convert celsius to fahrenheit.
+# c= 5*f-32/9
+def f_to_c(f):
+    return 5*(f-32)/9
+
+    
+f=int(input('Enter temperature in F:'))
+c=f_to_c(f)
+print(f'{ round(c,2)} ℃')
