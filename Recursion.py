6cb3c20bfcd5058f3ee931c665b9069a1631a0d1
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-27 17:27:14 +0500

    modified
    added comments

--- Recursion.py
+++ Recursion.py
@@ -1,3 +1,7 @@
+# Python also accepts function recursion, which means a defined function can call itself.
+# Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result.
+
+
 #Recursion is a programming technique where a function calls itself to solve a problem.
 #Instead of using loops, recursion breaks a problem into smaller sub-problems of the same type and solves them using repeated function calls.
 
