17134829223218e98b7d66c0ac89645499e6c0fb
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-27 20:16:51 +0500

    added

---
+++ file_write.py
@@ -0,0 +1,5 @@
+st = 'A thing of beauty is a joy for ever!'
+f=open('myfile.txt', 'w')
+
+f.write(st)
+f.close()
---
+++ regex_module.py
@@ -0,0 +1,46 @@
+# A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
+# RegEx can be used to check if a string contains the specified search pattern.
+
+#RegEx Module
+#Python has a built-in package called re, which can be used to work with Regular Expressions.
+#Import the re module:
+
+
+#ExampleGet your own Python Server
+#Search the string to see if it starts with "The" and ends with "Spain":
+import re
+txt = 'The rain in Spain'
+x = re.search("^The.*Spain$", txt)
+print(x)
+
+# RegEx Functions
+#The re module offers a set of functions that allows us to search a string for a match:
+
+
+
+
+#           Function	     Description
+
+#           search	         Returns a Match object if there is a match anywhere in the string
+#           split	         Returns a list where the string has been split at each match
+#           findall	         Returns a list containing all matches
+#           sub	             Replaces one or many matches with a string
+
+
+
+
+# Metacharacters
+# Metacharacters are characters with a special meaning:
+
+#      Character	Description	                                                                             Example	
+#       []       	A set of characters	                                                                     "[a-m]"	
+#       \       	Signals a special sequence (can also be used to escape special characters)	             "\d"	
+#       .       	Any character (except newline character)	                                             "he..o"	
+#       ^       	Starts with	                                                                             "^hello"	
+#       $       	Ends with	                                                                             "planet$"	
+#       *       	Zero or more occurrences	                                                             "he.*o"	
+#       +       	One or more occurrences	                                                                 "he.+o"	
+#       ?       	Zero or one occurrences	                                                                 "he.?o"	
+#       {}       	Exactly the specified number of occurrences	                                             "he.{2}o"	
+#       |       	Either or	                                                                             "falls|stays"	
+#       ()       	Capture and group
