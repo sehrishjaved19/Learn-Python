3252e0107608fe0f8436ac58ff1c2441632ab680
Sehrish Javed <sehrishjaved119@gmail.com>
2025-07-27 16:47:56 +0500

    added match_case.py

---
+++ match_case.py
@@ -0,0 +1,16 @@
+day = int(input('Enter your choice (1 to 7): '))
+match day:
+    case 1:
+        print('Monday')
+    case 2:
+        print('Tuesday')
+    case 3:
+        print('WedNESday')
+    case 4:
+        print('Thursday')
+    case 5:
+        print('Friday')
+    case 6:
+        print('Saturday')
+    case 7:
+        print('Sunday')
