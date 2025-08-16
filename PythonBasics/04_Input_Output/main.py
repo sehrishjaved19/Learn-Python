c6d46e9cd74a845a3d63a0f47ccdd4d130efc4a9
Your Name <email@example.com>
2025-08-16 20:29:06 +0500

    Added

---
+++ PythonBasics/04_Input_Output/main.py
@@ -0,0 +1,33 @@
+# Input--> Input() returns string value.
+
+name = input("Enter Your Name: ")
+print("Hello, "+name+"!")
+
+# convert data type in input function
+age = int(input("Enter Your Age: "))
+print("You are "+str(age)+" Years old.") 
+# its necessary to convert the data typr to string because we cannot concatenate interger with string.
+
+
+# Output--> use print() function
+print("Hello, World!")
+
+# using string formating
+name = "Sehrish"
+age = 19
+print(f"My name is {name} and I am {age} years old.")
+
+
+# Typecasting--> converting datatype
+num_str = "123"
+num_int = int(num_str)
+print(num_int + 1)
+
+# Comments--> use to add notes
+# This is a single_line comment!
+''' this is a multi-line 
+comment
+that spans multiple lines.'''
+
+
+
